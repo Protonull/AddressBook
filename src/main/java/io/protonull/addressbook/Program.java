efde1c528f5b7e2ca0adda02ae470aed0afa3982
Alexander <52572989+Protonull@users.noreply.github.com>
2019-09-02 15:12:17 +0100

    Removed slf4j
    It's great for information but not so much for text interfaces

--- src/main/java/io/protonull/addressbook/Program.java
+++ src/main/java/io/protonull/addressbook/Program.java
@@ -5,14 +5,11 @@ import io.protonull.addressbook.api.IMenu;
 import io.protonull.addressbook.impl.menus.MainMenu;
 import io.protonull.addressbook.impl.json.JSONAddressBook;
 import io.protonull.addressbook.utilities.ConsoleUtilities;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.util.Scanner;
 
 public final class Program {
 
-    private static Logger logger = LoggerFactory.getLogger(Program.class);
 
     public static IAddressBook addressBook;
     public static IMenu menu = null;
